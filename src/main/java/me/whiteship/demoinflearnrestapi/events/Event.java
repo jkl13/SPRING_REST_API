cb370ea144235b26bc4a1231dc650068af763775
이준규 <jk6033@sparrowfasoo.com>
2023-03-16 16:52:57 +0900

    project setup

---
+++ src/main/java/me/whiteship/demoinflearnrestapi/events/Event.java
@@ -0,0 +1,26 @@
+package me.whiteship.demoinflearnrestapi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int MaxPrice;
+    private int limitOfeEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+
+}
